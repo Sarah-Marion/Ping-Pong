749666dd40f92fab7a7861335c8440ab85e2a910
Sarah-Marion <devsarahmarion@gmail.com>
2018-03-29 11:55:40 +0300

    creation of the front-end

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,15 @@ var condition = function(number) {
   } else answer = number;
   return answer;
 }
+
+//user input
+
+$(document).ready(function() {
+  var userInput = parseInt(prompt("Enter number of tries:  "))
+
+  for (index = 1; index <= userInput; index++) {
+    theResult = condition(index);
+    $("ul").append(+theResult+");
+  }
+
+});
